86a073a1105e5a2d0c743c09f13888ef63723573
gaoming <Administrator@PC-20170308PKRS>
2020-10-19 16:06:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# sqoop-1.4.7-atlas-2.1.0-hdp-3.1.4-hylink" 
---
+++ bin/codegen_t_ppp.java
@@ -0,0 +1,327 @@
+// ORM class for table 't_ppp'
+// WARNING: This class is AUTO-GENERATED. Modify at your own risk.
+//
+// Debug information:
+// Generated date: Mon Oct 19 15:32:39 CST 2020
+// For connector: org.apache.sqoop.manager.MySQLManager
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.lib.db.DBWritable;
+import org.apache.sqoop.lib.JdbcWritableBridge;
+import org.apache.sqoop.lib.DelimiterSet;
+import org.apache.sqoop.lib.FieldFormatter;
+import org.apache.sqoop.lib.RecordParser;
+import org.apache.sqoop.lib.BooleanParser;
+import org.apache.sqoop.lib.BlobRef;
+import org.apache.sqoop.lib.ClobRef;
+import org.apache.sqoop.lib.LargeObjectLoader;
+import org.apache.sqoop.lib.SqoopRecord;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+public class codegen_t_ppp extends SqoopRecord  implements DBWritable, Writable {
+  private final int PROTOCOL_VERSION = 3;
+  public int getClassFormatVersion() { return PROTOCOL_VERSION; }
+  public static interface FieldSetterCommand {    void setField(Object value);  }  protected ResultSet __cur_result_set;
+  private Map<String, FieldSetterCommand> setters = new HashMap<String, FieldSetterCommand>();
+  private void init0() {
+    setters.put("id", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        codegen_t_ppp.this.id = (Integer)value;
+      }
+    });
+    setters.put("pice", new FieldSetterCommand() {
+      @Override
+      public void setField(Object value) {
+        codegen_t_ppp.this.pice = (java.math.BigDecimal)value;
+      }
+    });
+  }
+  public codegen_t_ppp() {
+    init0();
+  }
+  private Integer id;
+  public Integer get_id() {
+    return id;
+  }
+  public void set_id(Integer id) {
+    this.id = id;
+  }
+  public codegen_t_ppp with_id(Integer id) {
+    this.id = id;
+    return this;
+  }
+  private java.math.BigDecimal pice;
+  public java.math.BigDecimal get_pice() {
+    return pice;
+  }
+  public void set_pice(java.math.BigDecimal pice) {
+    this.pice = pice;
+  }
+  public codegen_t_ppp with_pice(java.math.BigDecimal pice) {
+    this.pice = pice;
+    return this;
+  }
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof codegen_t_ppp)) {
+      return false;
+    }
+    codegen_t_ppp that = (codegen_t_ppp) o;
+    boolean equal = true;
+    equal = equal && (this.id == null ? that.id == null : this.id.equals(that.id));
+    equal = equal && (this.pice == null ? that.pice == null : this.pice.equals(that.pice));
+    return equal;
+  }
+  public boolean equals0(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof codegen_t_ppp)) {
+      return false;
+    }
+    codegen_t_ppp that = (codegen_t_ppp) o;
+    boolean equal = true;
+    equal = equal && (this.id == null ? that.id == null : this.id.equals(that.id));
+    equal = equal && (this.pice == null ? that.pice == null : this.pice.equals(that.pice));
+    return equal;
+  }
+  public void readFields(ResultSet __dbResults) throws SQLException {
+    this.__cur_result_set = __dbResults;
+    this.id = JdbcWritableBridge.readInteger(1, __dbResults);
+    this.pice = JdbcWritableBridge.readBigDecimal(2, __dbResults);
+  }
+  public void readFields0(ResultSet __dbResults) throws SQLException {
+    this.id = JdbcWritableBridge.readInteger(1, __dbResults);
+    this.pice = JdbcWritableBridge.readBigDecimal(2, __dbResults);
+  }
+  public void loadLargeObjects(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void loadLargeObjects0(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void write(PreparedStatement __dbStmt) throws SQLException {
+    write(__dbStmt, 0);
+  }
+
+  public int write(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeInteger(id, 1 + __off, 4, __dbStmt);
+    JdbcWritableBridge.writeBigDecimal(pice, 2 + __off, 3, __dbStmt);
+    return 2;
+  }
+  public void write0(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeInteger(id, 1 + __off, 4, __dbStmt);
+    JdbcWritableBridge.writeBigDecimal(pice, 2 + __off, 3, __dbStmt);
+  }
+  public void readFields(DataInput __dataIn) throws IOException {
+this.readFields0(__dataIn);  }
+  public void readFields0(DataInput __dataIn) throws IOException {
+    if (__dataIn.readBoolean()) { 
+        this.id = null;
+    } else {
+    this.id = Integer.valueOf(__dataIn.readInt());
+    }
+    if (__dataIn.readBoolean()) { 
+        this.pice = null;
+    } else {
+    this.pice = org.apache.sqoop.lib.BigDecimalSerializer.readFields(__dataIn);
+    }
+  }
+  public void write(DataOutput __dataOut) throws IOException {
+    if (null == this.id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.id);
+    }
+    if (null == this.pice) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    org.apache.sqoop.lib.BigDecimalSerializer.write(this.pice, __dataOut);
+    }
+  }
+  public void write0(DataOutput __dataOut) throws IOException {
+    if (null == this.id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.id);
+    }
+    if (null == this.pice) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    org.apache.sqoop.lib.BigDecimalSerializer.write(this.pice, __dataOut);
+    }
+  }
+  private static final DelimiterSet __outputDelimiters = new DelimiterSet((char) 1, (char) 10, (char) 0, (char) 0, false);
+  public String toString() {
+    return toString(__outputDelimiters, true);
+  }
+  public String toString(DelimiterSet delimiters) {
+    return toString(delimiters, true);
+  }
+  public String toString(boolean useRecordDelim) {
+    return toString(__outputDelimiters, useRecordDelim);
+  }
+  public String toString(DelimiterSet delimiters, boolean useRecordDelim) {
+    StringBuilder __sb = new StringBuilder();
+    char fieldDelim = delimiters.getFieldsTerminatedBy();
+    __sb.append(FieldFormatter.escapeAndEnclose(id==null?"null":"" + id, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(pice==null?"null":pice.toPlainString(), delimiters));
+    if (useRecordDelim) {
+      __sb.append(delimiters.getLinesTerminatedBy());
+    }
+    return __sb.toString();
+  }
+  public void toString0(DelimiterSet delimiters, StringBuilder __sb, char fieldDelim) {
+    __sb.append(FieldFormatter.escapeAndEnclose(id==null?"null":"" + id, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(pice==null?"null":pice.toPlainString(), delimiters));
+  }
+  private static final DelimiterSet __inputDelimiters = new DelimiterSet((char) 1, (char) 10, (char) 0, (char) 0, false);
+  private RecordParser __parser;
+  public void parse(Text __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharSequence __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(byte [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(char [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(ByteBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  private void __loadFromFields(List<String> fields) {
+    Iterator<String> __it = fields.listIterator();
+    String __cur_str = null;
+    try {
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.id = null; } else {
+      this.id = Integer.valueOf(__cur_str);
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.pice = null; } else {
+      this.pice = new java.math.BigDecimal(__cur_str);
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  private void __loadFromFields0(Iterator<String> __it) {
+    String __cur_str = null;
+    try {
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.id = null; } else {
+      this.id = Integer.valueOf(__cur_str);
+    }
+
+    if (__it.hasNext()) {
+        __cur_str = __it.next();
+    } else {
+        __cur_str = "null";
+    }
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.pice = null; } else {
+      this.pice = new java.math.BigDecimal(__cur_str);
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  public Object clone() throws CloneNotSupportedException {
+    codegen_t_ppp o = (codegen_t_ppp) super.clone();
+    return o;
+  }
+
+  public void clone0(codegen_t_ppp o) throws CloneNotSupportedException {
+  }
+
+  public Map<String, Object> getFieldMap() {
+    Map<String, Object> __sqoop$field_map = new HashMap<String, Object>();
+    __sqoop$field_map.put("id", this.id);
+    __sqoop$field_map.put("pice", this.pice);
+    return __sqoop$field_map;
+  }
+
+  public void getFieldMap0(Map<String, Object> __sqoop$field_map) {
+    __sqoop$field_map.put("id", this.id);
+    __sqoop$field_map.put("pice", this.pice);
+  }
+
+  public void setField(String __fieldName, Object __fieldVal) {
+    if (!setters.containsKey(__fieldName)) {
+      throw new RuntimeException("No such field:"+__fieldName);
+    }
+    setters.get(__fieldName).setField(__fieldVal);
+  }
+
+}
